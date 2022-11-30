c1747e9edd7f7f3ce99b463172884f32cd791711
Netherzapdos <netherzapdos@gmail.com>
2022-11-30 09:05:40 +0800

    [NEW] Added Basic Proximity Sensor Between Character and Axolotl/Game Object

--- src/Game.ts
+++ src/Game.ts
@@ -5,8 +5,10 @@ import Map from "./components/map/Map"
 
 export default class Game extends Phaser.Scene 
 {
-	char!: any; 
+	char!: Character;
 	map!: Map;
+	axl!: Axolotl; 
+
 
     
 
@@ -21,21 +23,18 @@ export default class Game extends Phaser.Scene
   
     create() 
 	{
-
-		
-
-
 	// Create Map
 		this.map = new Map(this); 
 
 	// Create character
 		this.char = new Character(this, 440, 340);	// Classes should be declared with "this.x" first, 
 		this.char.anims(); 							// then declare the variable at the top of this scene (sample for char above)
-													// this allows the Class to be then used anywhere here. 
+													// this allows the Class to be used in global scope.
 
 	// Create Axolotl
-		var yellowAxl = new Axolotl(this, 540, 450); // Declare axl in this scene and pass details to Axolotls Class
-		yellowAxl.playAnim('wild_yellow', 'axl_idle', 0, 5);	// Play animation
+		this.axl = new Axolotl(this, 540, 450); 			// Declare axl in this scene and pass details to Axolotls Class
+		this.axl.playAnim('wild_yellow', 'axl_idle', 0, 5);	// Play animation
+		// ** Redo this setup to be similar to Character class **
 
 	// Create Custom Cursor/Mouse Pointer
 		this.input.setDefaultCursor('url(/images/cursors/main_cursor.cur), pointer'); 
@@ -45,5 +44,24 @@ export default class Game extends Phaser.Scene
 	update() 
 	{
 		this.char.move(); 
+
+		var dist = Phaser.Math.Distance.Between(this.char.getPositionX(), this.char.getPositionY(), this.axl.getPositionX(), this.axl.getPositionY());
+		console.log(dist); 
+
+		if (dist > 100) {
+			console.log('more than 100')
+		}
+		else {
+			console.log('less than 100')
+		}
+
+
+		// It is finally working - now just need to only enable this whenever the character is moving, and not always updating
+		// Detection of how much the distance is, is also working. 
+		// i.e. in Axolotl Class. Maybe if you're about to click the axolotl, check first if the player sprite is near enough to
+		// the axolotl itself? If not, do nothing. If it is, invoke. 
+
+	
+		
 	}
 }
