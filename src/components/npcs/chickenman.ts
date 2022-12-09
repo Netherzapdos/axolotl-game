b50f923decfd21d405be65432d62ac8043869cf0
Netherzapdos <netherzapdos@gmail.com>
2022-12-09 21:08:16 +0800

    [POLISH] Adjusted Chicken Man NPC Proximity Interactions to use the New Re-Usable Sensors Class

--- src/components/npcs/chickenman.ts
+++ src/components/npcs/chickenman.ts
@@ -10,9 +10,7 @@ export default class ChickenMan{
     {
         var Bodies = scene.matter.bodies; 
 
-        const { width, height } = scene.scale; 
-
-        this.chickenMan = scene.matter.add.sprite(340, 340, 'chicken_hitman', null, { label: 'chickenMan', isStatic: true, circleRadius: 20 })
+        this.chickenMan = scene.matter.add.sprite(340, 340, 'chicken_man', null, { label: 'chickenMan', isStatic: true, circleRadius: 20 })
 
 
             // Change on-hover to chat cursor
@@ -23,93 +21,18 @@ export default class ChickenMan{
                 }
             ); 
 
-            // Enable proximity-sensitive chat
-            var proximityCheck = false;
+            // Create proximity interactions
+            var chickenSensor = new Sensor();
+            chickenSensor.interactionRangeDialogueModal(scene, this.chickenMan, 'Chicken Man', 'Who are you?', 'Where am I?', 'How the heck did I', 'get here...');
+            chickenSensor.withinRangeMsg(scene, this.chickenMan, 'chickenMan', 'My head hurts...'); 
+            chickenSensor.exitRangeMsg(scene, 'chickenMan'); 
 
-            var outsideRange = new Sensor();
-            outsideRange.outsideRange(scene, this.chickenMan)
-
-            // this.chickenMan.on('pointerdown', () => {
-            //     if (proximityCheck == false) {
-                    
-            //         let outsideRange = scene.add.text(this.chickenMan.x - 25, this.chickenMan.y - 15, 'Too far').setTint(0xff0000).setFontSize(8).setFontFamily('PressStart2P').setResolution(5);
-                
-            //         scene.time.delayedCall(1000, () => {
-            //             scene.add.tween({
-            //                 targets: outsideRange,
-            //                 alpha: 0,
-            //                 duration: 1000,
-            //                 ease: 'Power2'
-            //             })
-            //         })
-            //     } else if (proximityCheck == true) {
-
-            //         let dialogue = new Modal(scene, width * 0.5, height * 0.86)
-            //         dialogue.setName(scene, 'Chicken Man');
-            //         dialogue.firstLine(scene, 'Who are you?');
-            //         dialogue.secondLine(scene, 'Where am i?');
-            //         dialogue.thirdLine(scene, 'How the heck did I');
-            //         dialogue.fourthLine(scene, 'get here...')
-
-            //         // ** continue building **
-            //     }
-            // })
-
-        // Create sensors
-        scene.matter.world.on('collisionstart', (event: any) => {
-
-            var pairs = event.pairs;
-
-            for (var i = 0; i < pairs.length; i++)
-            {
-                var bodyA = pairs[i].bodyA;
-                var bodyB = pairs[i].bodyB;
-
-                if (pairs[i].isSensor)
-
-                {
-                    if (bodyA.label === 'playerSensor' && bodyB.label === 'chickenMan')
-                    {
-                        let withinRange = scene.add.text(this.chickenMan.x - 45, this.chickenMan.y - 15, "My head hurts...").setTint().setFontSize(8).setFontFamily('PressStart2P').setResolution(5);
-                    
-                        scene.time.delayedCall(1000, () => {
-                            scene.add.tween({
-                                targets: withinRange,
-                                alpha: 0,
-                                duration: 1000,
-                                ease: 'Power2'
-                            })
-                        })
-                        proximityCheck = true;
-                    }
-                }
-            }
-        })
-        scene.matter.world.on('collisionend', (event: any) => 
-        {
-            var pairs = event.pairs;
-
-            for (var i = 0; i < pairs.length; i++)
-            {
-                var bodyA = pairs[i].bodyA;
-                var bodyB = pairs[i].bodyB;
-
-                if (pairs[i].isSensor)
-                {
-                    if (bodyA.label === 'playerSensor' && bodyB.label === 'chickenMan')
-                    {
-                        proximityCheck = false;
-                    }
-                }
-            }
-        })
-
-
+            
         // Create Idle Anim
 			this.chickenMan.anims.create({
 				key: 'idle',
 				frames: this.chickenMan.anims.generateFrameNumbers(
-					'chicken_hitman', { 
+					'chicken_man', { 
 						start: 0, 
 						end: 8
 					}),
